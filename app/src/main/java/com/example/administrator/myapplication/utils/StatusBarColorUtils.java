292031b00880beaa16d354e114d33a1f73767097
zhangzeyang <zhangjie@huidr.com>
2019-10-29 17:09:41 +0800

    新建ActivitySecond

--- app/src/main/java/com/example/administrator/myapplication/utils/StatusBarColorUtils.java
+++ app/src/main/java/com/example/administrator/myapplication/utils/StatusBarColorUtils.java
@@ -15,11 +15,13 @@ public class StatusBarColorUtils {
      * 修改状态栏颜色需要先设置activity 透明状态栏
      * getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//设置字体黑色
      * getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);//设置状态栏字体白色
+     *
      * */
-    @SuppressLint("InlinedApi")
+
     public static void changeStatusBarColor(Activity activity, int colorId) {
         if (Build.VERSION.SDK_INT > 21) {
             activity.getWindow().setStatusBarColor(activity.getResources().getColor(colorId));
+
             if (colorId == R.color.colortran) {
                 activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             } else {
