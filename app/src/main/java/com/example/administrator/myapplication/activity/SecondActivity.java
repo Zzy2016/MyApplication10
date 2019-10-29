869a389d93c67657fcacbd95e155fa68dcdbaf31
zhangzeyang <zhangjie@huidr.com>
2019-10-29 17:18:14 +0800

    新建branches

--- app/src/main/java/com/example/administrator/myapplication/activity/SecondActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/SecondActivity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.administrator.myapplication.R;
+
 /*
-*  new  branches
-* */
+ *  new  branches
+ * */
 public class SecondActivity extends AppCompatActivity {
 
     @Override
