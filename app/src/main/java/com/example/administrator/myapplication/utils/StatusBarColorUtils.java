9e34f86e319c181868fabeaecf67b74eedbb1060
zhangzeyang <zhangjie@huidr.com>
2019-10-29 16:56:22 +0800

    添加注释

--- app/src/main/java/com/example/administrator/myapplication/utils/StatusBarColorUtils.java
+++ app/src/main/java/com/example/administrator/myapplication/utils/StatusBarColorUtils.java
@@ -8,8 +8,12 @@ import com.example.administrator.myapplication.R;
 
 public class StatusBarColorUtils {
 
-    private Context context;
-
+    /*
+     * 修改状态栏颜色
+     * 修改状态栏颜色需要先设置activity 透明状态栏
+     * getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//设置字体黑色
+     * getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);//设置状态栏字体白色
+     * */
     public static void changeStatusBarColor(Activity activity, int colorId) {
         if (Build.VERSION.SDK_INT > 21) {
             activity.getWindow().setStatusBarColor(activity.getResources().getColor(colorId));
